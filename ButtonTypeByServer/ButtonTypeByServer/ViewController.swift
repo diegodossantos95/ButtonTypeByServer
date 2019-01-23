d27088d542c72e91323f5410e510b295ac34f973
dos Santos, Diego <diego.dos.santos@sap.com>
2019-01-23 00:49:25 -0200

    Created base project

---
+++ ButtonTypeByServer/ButtonTypeByServer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ButtonTypeByServer
+//
+//  Created by dos Santos, Diego on 23/01/19.
+//  Copyright © 2019 Diego. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
